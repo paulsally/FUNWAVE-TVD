7e613a65b0c56085fefe423957e897927062ebb4
Sara Polselli <paulsally@gmail.com>
2024-07-12 19:14:58 +0200

    Edited matlab script so that: 1. It plots an image for each output file; 2. It saves the images inside ./output/m

--- simple_cases/surface_wave_1d/postprocessing/plot_wave.m
+++ simple_cases/surface_wave_1d/postprocessing/plot_wave.m
@@ -22,15 +22,25 @@ len=4;
 set(gcf,'units','inches','paperunits','inches','papersize', [wid len],'position',[1 1 wid len],'paperposition',[0 0 wid len]);
 clf
 
-eta=load([fdir 'eta_00003'], "-ascii");
-plot(x,-dep,'k',x,eta,'b','LineWidth',2)
-hold on
-plot(x_wm,yy,'r')
-text(x_wm(2),0.6,'wavemaker','Color','r','LineWidth',2)
-plot(x_sponge,yy,'k')
-text(x_sponge(1)+20,0.6,'sponge layer','Color','k','LineWidth',2)
-axis([0 1024 -1 1])
-grid
-xlabel('x(m)')
-ylabel('eta(m)')
+outputDir = [fdir '../postprocessing/output/m'];
+files = dir(strcat(fdir, 'eta_*'));
 
+close all;
+for i = 1:numel(files)
+    fig = figure;
+    disp(['Processing file ', num2str(i), ': ', files(i).name]);
+    eta=load([fdir files(i).name], "-ascii");
+    plot(x,-dep,'k',x,eta,'b','LineWidth',2)
+    hold on
+    plot(x_wm,yy,'r')
+    text(x_wm(2),0.6,'wavemaker','Color','r','LineWidth',2)
+    plot(x_sponge,yy,'k')
+    text(x_sponge(1)+20,0.6,'sponge layer','Color','k','LineWidth',2)
+    axis([0 1024 -1 1])
+    grid
+    xlabel('x(m)')
+    ylabel('eta(m)')
+    outputFile = fullfile(outputDir, sprintf('eta_1d_wave_%s.png', files(i).name));
+    saveas(fig, outputFile);
+    %close(fig);
+end
